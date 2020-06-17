5e1b837794a1ded28ffd3d2ef6f862dc29def498
sonyalazarova <sonyaslazarova@gmail.com>
2020-06-17 15:49:16 +0300

    Updates

---
+++ precache-manifest.f9b61ec7e07115d270281242bf0ff706.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c3ef17a08ee17bfab26a76bfb29353c",
+    "url": "/monster-progect-ReactBasics/index.html"
+  },
+  {
+    "revision": "3d1fb82616d5ae446d0a",
+    "url": "/monster-progect-ReactBasics/static/css/main.1a6957dd.chunk.css"
+  },
+  {
+    "revision": "cc86f92a0e9b1ed7b6f7",
+    "url": "/monster-progect-ReactBasics/static/js/2.1b35cb52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-progect-ReactBasics/static/js/2.1b35cb52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d1fb82616d5ae446d0a",
+    "url": "/monster-progect-ReactBasics/static/js/main.8435baff.chunk.js"
+  },
+  {
+    "revision": "a5759458170fc6ebde57",
+    "url": "/monster-progect-ReactBasics/static/js/runtime-main.fb36df07.js"
+  }
+]);
